3ca314cc461e8659c97d76f638375cded56f5b50
Sandax <91079659+Sandaxxx@users.noreply.github.com>
2023-01-21 20:59:17 +0100

    Add files via upload

---
+++ card.py
@@ -0,0 +1,42 @@
+# C:\Users\sandax\Desktop\dev\#ico_image\png\servers.png
+
+import os
+from PIL import Image
+from colorama import Fore
+from pystyle import Colors, Colorate, Write
+
+
+banner = """
+                       ██████╗ ██████╗ ███╗   ██╗██╗   ██╗███████╗██████╗ ████████╗███████╗██████╗ 
+                      ██╔════╝██╔═══██╗████╗  ██║██║   ██║██╔════╝██╔══██╗╚══██╔══╝██╔════╝██╔══██╗
+                      ██║     ██║   ██║██╔██╗ ██║██║   ██║█████╗  ██████╔╝   ██║   █████╗  ██████╔╝
+                      ██║     ██║   ██║██║╚██╗██║╚██╗ ██╔╝██╔══╝  ██╔══██╗   ██║   ██╔══╝  ██╔══██╗
+                      ╚██████╗╚██████╔╝██║ ╚████║ ╚████╔╝ ███████╗██║  ██║   ██║   ███████╗██║  ██║
+                       ╚═════╝ ╚═════╝ ╚═╝  ╚═══╝  ╚═══╝  ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚══════╝╚═╝  ╚═╝
+
+                                 ─══════════════════════════☆☆══════════════════════════─
+                                                Developper : https://github.com/Sandaxxx
+                                 ─══════════════════════════☆☆══════════════════════════─
+
+
+
+                                                                                                     """
+
+def converter_png_ico(filename_png):
+
+    try:
+        file = Image.open(filename_png)
+        ico_image = file.resize((400, 400))
+
+        ico_image.save('image_file.ico')
+        print(Fore.BLUE+"[!]"+Fore.WHITE+" >"+Fore.GREEN+" Successfully Converted!")
+        
+    except FileNotFoundError:
+        print(Fore.BLUE+"[X]"+Fore.WHITE+" >"+Fore.RED+" Error File Path")
+
+
+if __name__ == "__main__":
+    print(Colorate.Horizontal(Colors.purple_to_blue, banner))
+    path_png = Write.Input("[+] File path > ", Colors.red_to_purple, interval=0.0025)
+    
+    converter_png_ico(path_png)
